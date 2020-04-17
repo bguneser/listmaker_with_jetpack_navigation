19e26df32313a9cdd14b71ec2b676be35d494c58
bguneser <bguneser@ford.com.tr>
2020-04-17 16:13:15 +0300

    Espresso deprecated kütüphane düzeltildi

---
+++ app/src/androidTest/java/com/bguneser/listmaker/MainActivityTest.kt
@@ -0,0 +1,167 @@
+package com.bguneser.listmaker
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import com.microsoft.appcenter.espresso.Factory
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.After
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import com.microsoft.appcenter.espresso.Factory.getReportHelper
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest() {
+        val recyclerView = onView(
+            allOf(
+                withId(R.id.lists_recyclerview),
+                childAtPosition(
+                    childAtPosition(
+                        IsInstanceOf.instanceOf(android.view.ViewGroup::class.java),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        recyclerView.check(matches(isDisplayed()))
+
+        val recyclerView2 = onView(
+            allOf(
+                withId(R.id.lists_recyclerview),
+                childAtPosition(
+                    childAtPosition(
+                        IsInstanceOf.instanceOf(android.view.ViewGroup::class.java),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        recyclerView2.check(matches(isDisplayed()))
+
+        val recyclerView3 = onView(
+            allOf(
+                withId(R.id.lists_recyclerview),
+                childAtPosition(
+                    childAtPosition(
+                        IsInstanceOf.instanceOf(android.view.ViewGroup::class.java),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        recyclerView3.check(matches(isDisplayed()))
+
+        val recyclerView4 = onView(
+            allOf(
+                withId(R.id.lists_recyclerview),
+                childAtPosition(
+                    childAtPosition(
+                        IsInstanceOf.instanceOf(android.view.ViewGroup::class.java),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        recyclerView4.check(matches(isDisplayed()))
+
+        val floatingActionButton = onView(
+            allOf(
+                withId(R.id.fab),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.nav_host_fragment),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        floatingActionButton.perform(click())
+
+        val floatingActionButton2 = onView(
+            allOf(
+                withId(R.id.fab),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.nav_host_fragment),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        floatingActionButton2.perform(click())
+
+        val floatingActionButton3 = onView(
+            allOf(
+                withId(R.id.fab),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.nav_host_fragment),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        floatingActionButton3.perform(click())
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+
+    @Rule
+    var reportHelper = Factory.getReportHelper()
+
+    @After
+    fun TearDown() {
+        reportHelper.label("Stopping App")
+    }
+}
